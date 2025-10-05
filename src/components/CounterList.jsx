722c2cfe8b41e6d6c0d607d290650e143148da64
Ashley W <aw24681@yahoo.com>
2025-10-05 13:26:28 -0700

    Updated CounterList.jsx with more stylistic elements

--- src/components/CounterList.jsx
+++ src/components/CounterList.jsx
@@ -2,9 +2,15 @@
 	// It loops through each counter (c) and creates one <Counter> component for each.
     // It doesn’t own any data itself — it just renders whatever is passed in
 
-import Counter from "./Counter.jsx";
+import Counter from "./Counter.jsx"; // will not work until Counter.jsx is created
 
-export default function CounterList({ counters, onIncrement, onDecrement, onReset, onDelete, onRename }) {
+export default function CounterList({ 
+    counters, 
+    onIncrement, 
+    onDecrement, 
+    onReset, 
+    onDelete, 
+    onRename }) {
   // export default means this is the main thing exported from this file to be used in other files
     // Receives props from parent (App.jsx): counters array + handler functions
     if (!counters.length) {
@@ -13,7 +19,7 @@ export default function CounterList({ counters, onIncrement, onDecrement, onRese
   }
   // else, if there ARE counters, render a Counter component for each counter object:
   return (
-    <section>
+    <section style={{ display: "grid", gap: 12 }}>
       {counters.map((c) => (
         // counters is an array of counter objects from App.jsx state 
         // .map() loops through the array, returns a Counter component for each item
